256676bb55be4ba81769c4ae034d548e263ff5de
Student <student@Makerss-MacBook-Pro-2.local>
2019-09-04 14:00:19 +0100

    updated the battle class to include a get route and created rspec

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Battle
+This application can be run directly from the app.rb (sinatra) or by typing rackup in the console (which will run via the config.ru file)
--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
   get '/' do
-    'Hello Battle'
+    "Hello World"
   end
   # start the server if ruby file executed directly
   run! if app_file == $0
