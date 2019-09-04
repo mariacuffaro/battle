e4b622e07bce51d3ff2ff9c74603938c514f0f57
Student <student@Makerss-MacBook-Pro-2.local>
2019-09-04 15:23:38 +0100

    added new form to enter player names and feature tests to test fom

--- app.rb
+++ app.rb
@@ -7,4 +7,12 @@ class Battle < Sinatra::Base
   end
   # start the server if ruby file executed directly
   run! if app_file == $0
+
+  get '/enter-players' do
+    erb(:index)
+  end
+
+  post '/names' do
+    erb(:play)
+  end
 end
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,6 +1,21 @@
-feature 'Testing infrastructure', type: :feature do
+feature 'Testing infrastructure' do
   scenario 'homepage displays expected text' do
     visit('/')
     expect(page).to have_content 'Testing infrastructure working!'
+  end
+end
+
+feature 'Testing form' do
+  scenario 'form exists on the page' do
+    visit('/enter-players')
+    page.should have_css('input[name="Player1"]')
+    page.should have_css('input[name="Player2"]')
+  end
+  scenario 'players can use the form to input thier names' do
+    visit('/enter-players')
+    fill_in('Player1', with: 'Karlo')
+    fill_in('Player2', with: 'Maria')
+    click_button('Start Battle')
+    expect(page).to have_content 'Karlo and Maria - Fight!'
   end
 end
